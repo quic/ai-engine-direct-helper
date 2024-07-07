cfb35948e799bad544e1bf0535e482f2e040f95b
quic-zhanweiw <150764245+quic-zhanweiw@users.noreply.github.com>
2024-07-07 09:06:29 +0800

    Delete QNNHelper directory
    Signed-off-by: quic-zhanweiw <150764245+quic-zhanweiw@users.noreply.github.com>

--- QNNHelper/__init__.py
+++
@@ -1,9 +0,0 @@
-#=============================================================================
-#
-# Copyright (c) 2023, Qualcomm Innovation Center, Inc. All rights reserved.
-# 
-# SPDX-License-Identifier: BSD-3-Clause
-#
-#=============================================================================
-
-from .qnncontext import *
