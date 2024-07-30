ced4596eeeff1042d21a8c5706137d9a19b3d606
quic-zhanweiw <150764245+quic-zhanweiw@users.noreply.github.com>
2024-07-30 19:51:00 +0800

    Update setup.py
    Signed-off-by: quic-zhanweiw <150764245+quic-zhanweiw@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -27,9 +27,17 @@ CONFIG = "Release"  # Release, RelWithDebInfo
 package_name = "qai_appbuilder"
 
 machine = platform.machine()
+generate = "-G \"Visual Studio 17 2022\""
 arch = "ARM64"
+
 if machine == "AMD64":
     arch = "ARM64EC"
+generate += " -A " + arch
+
+if machine == "aarch64":
+    arch = "aarch64"
+    generate = ""
+
 print("-- Arch: " + arch)
 
 python_path = "script"
@@ -37,6 +45,8 @@ binary_path = python_path + "/" + package_name
 PACKAGE_ZIP  = "QAI_AppBuilder-win_arm64-QNN" + VERSION + "-" + CONFIG + ".zip"
 if arch == "ARM64EC":
     PACKAGE_ZIP  = "QAI_AppBuilder-win_arm64ec-QNN" + VERSION + "-" + CONFIG + ".zip"
+elif arch == "aarch64":
+    PACKAGE_ZIP  = "QAI_AppBuilder-linux_arm64-QNN" + VERSION + "-" + CONFIG + ".zip"
 
 def zip_package(dirpath, outFullName):
     zip = zipfile.ZipFile(outFullName, "w", zipfile.ZIP_DEFLATED)
@@ -50,26 +60,30 @@ def build_clean():
     shutil.rmtree(python_path + "/qai_appbuilder.egg-info")
     shutil.rmtree("build")
     shutil.rmtree("lib")
-    os.remove(binary_path + "/libappbuilder.dll")
-    os.remove(binary_path + "/QAIAppSvc.exe")
+    if os.path.exists(binary_path + "/QAIAppSvc.exe"):
+        os.remove(binary_path + "/libappbuilder.dll")
+        os.remove(binary_path + "/QAIAppSvc.exe")
     if os.path.exists(binary_path + "/libappbuilder.pdb"):
         os.remove(binary_path + "/libappbuilder.pdb")
+    if os.path.exists(binary_path + "/libappbuilder.so"):
+        os.remove(binary_path + "/libappbuilder.so")
 
 def build_cmake():
     if not os.path.exists("build"):
         os.mkdir("build")
     os.chdir("build")
 
-    vs_version = " \"Visual Studio 17 2022\""
-
-    subprocess.run("cmake .. -G " + vs_version + " -A " + arch)
-    subprocess.run("cmake --build ./ --config " + CONFIG)
+    subprocess.run("cmake .. " + generate, shell=True)
+    subprocess.run("cmake --build ./ --config " + CONFIG, shell=True)
     os.chdir("../")
 
-    shutil.copy("lib/" + CONFIG +"/libappbuilder.dll", binary_path)
-    shutil.copy("lib/" + CONFIG + "/QAIAppSvc.exe", binary_path)
+    if os.path.exists("lib/" + CONFIG + "/QAIAppSvc.exe"):
+        shutil.copy("lib/" + CONFIG +"/libappbuilder.dll", binary_path)
+        shutil.copy("lib/" + CONFIG + "/QAIAppSvc.exe", binary_path)
     if os.path.exists("lib/" + CONFIG + "/libappbuilder.pdb"):
         shutil.copy("lib/" + CONFIG + "/libappbuilder.pdb", binary_path)
+    if os.path.exists("lib/" + "libappbuilder.so"):
+        shutil.copy("lib/" + "libappbuilder.so", binary_path)
 
 build_cmake()
 
@@ -79,13 +93,19 @@ def build_release():
     include_path = "lib/package/include"
     if not os.path.exists(tmp_path):
         os.mkdir(tmp_path)
-    shutil.copy("lib/" + CONFIG + "/libappbuilder.dll", tmp_path)
-    shutil.copy("lib/" + CONFIG + "/libappbuilder.lib", tmp_path)
+
+    if os.path.exists("lib/" + CONFIG + "/QAIAppSvc.exe"):
+        shutil.copy("lib/" + CONFIG + "/libappbuilder.dll", tmp_path)
+        shutil.copy("lib/" + CONFIG + "/libappbuilder.lib", tmp_path)
+        shutil.copy("lib/" + CONFIG + "/QAIAppSvc.exe", tmp_path)
+
     if os.path.exists("lib/" + CONFIG + "/libappbuilder.pdb"):
         shutil.copy("lib/" + CONFIG + "/libappbuilder.pdb", tmp_path)
-    shutil.copy("lib/" + CONFIG + "/QAIAppSvc.exe", tmp_path)
     if os.path.exists("lib/" + CONFIG + "/QAIAppSvc.pdb"):
         shutil.copy("lib/" + CONFIG + "/QAIAppSvc.pdb", tmp_path)
+
+    if os.path.exists("lib/" + "/libappbuilder.so"):
+        shutil.copy("lib/" + "/libappbuilder.so", tmp_path)
 
     if not os.path.exists(include_path):
         os.mkdir(include_path)
@@ -107,17 +127,14 @@ class CMakeBuild(build_ext):
 
         cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
 
-        cmake_args = [
-            f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}{os.sep}",
-            f"-DPYTHON_EXECUTABLE={sys.executable}",
-            f"-DCMAKE_BUILD_TYPE={cfg}",  # not used on MSVC, but no harm
-        ]
-        build_args = []
-        if "CMAKE_ARGS" in os.environ:
-            cmake_args += [item for item in os.environ["CMAKE_ARGS"].split(" ") if item]
+        cmake_args = f" -DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}{os.sep}" + f" -DPYTHON_EXECUTABLE={sys.executable}" + f" -DCMAKE_BUILD_TYPE={cfg}"  # not used on MSVC, but no harm
+
+        build_args = ""
+        #if "CMAKE_ARGS" in os.environ:
+        #    cmake_args += [item for item in os.environ["CMAKE_ARGS"].split(" ") if item]
 
         # We pass in the version to C++. You might not need to.
-        cmake_args += [f"-DVERSION_INFO={self.distribution.get_version()}"]
+        cmake_args += f" -DVERSION_INFO={self.distribution.get_version()}"
 
         # Single config generators are handled "normally"
         single_config = any(x in cmake_generator for x in {"NMake", "Ninja"})
@@ -125,31 +142,29 @@ class CMakeBuild(build_ext):
         # CMake allows an arch-in-generator style for backward compatibility
         contains_arch = any(x in cmake_generator for x in {"ARM", "Win64"})
 
-        if not single_config and not contains_arch:
-            cmake_args += ["-A", arch]
+        if not single_config and not contains_arch and not arch == "aarch64":
+            cmake_args += " -A " + arch
 
         # Multi-config generators have a different way to specify configs
         if not single_config:
-            cmake_args += [
-                f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{cfg.upper()}={extdir}"
-            ]
-            build_args += ["--config", cfg]
+            cmake_args += f" -DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{cfg.upper()}={extdir}"
+            build_args += " --config " + cfg
 
         if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
             if hasattr(self, "parallel") and self.parallel:
                 # CMake 3.12+ only.
-                build_args += [f"-j{self.parallel}"]
+                build_args += f" -j{self.parallel}"
 
         build_temp = Path(self.build_temp) / ext.name
         if not build_temp.exists():
             build_temp.mkdir(parents=True)
 
-        subprocess.run(
-            ["cmake", ext.sourcedir, *cmake_args], cwd=build_temp, check=True
-        )
-        subprocess.run(
-            ["cmake", "--build", ".", *build_args], cwd=build_temp, check=True
-        )
+        print(cmake_args)
+        print(build_args)
+        print(ext.sourcedir)
+
+        subprocess.run("cmake " + ext.sourcedir + cmake_args, cwd=build_temp, check=True, shell=True)
+        subprocess.run("cmake --build . " + build_args, cwd=build_temp, check=True, shell=True)
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -159,7 +174,7 @@ setup(
     version=VERSION,
     packages=[package_name],
     package_dir={'': 'script'},
-    package_data={"": ["*.dll", "*.pdb", "*.exe"]},
+    package_data={"": ["*.dll", "*.pdb", "*.exe", "*.so"]},
     ext_modules=[CMakeExtension("qai_appbuilder.appbuilder", "pybind")],
     cmdclass={"build_ext": CMakeBuild},
     zip_safe=False,
