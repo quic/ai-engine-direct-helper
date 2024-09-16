5b36c9f602b93d498a8322cf6d4e3ae9cac4edfd
quic-zhanweiw <150764245+quic-zhanweiw@users.noreply.github.com>
2024-09-16 17:09:59 +0800

    Update BuildId.hpp
    Signed-off-by: quic-zhanweiw <150764245+quic-zhanweiw@users.noreply.github.com>

--- src/Utils/BuildId.hpp
+++ src/Utils/BuildId.hpp
@@ -11,7 +11,7 @@
 namespace qnn {
 namespace tools {
 
-inline std::string getBuildId() { return std::string("v2.24.0.240626131148_96320"); }
+inline std::string getBuildId() { return std::string("v2.26.0.240827110523_99241"); }
 
 }  // namespace tools
 }  // namespace qnn
