60ee370e689853cc1606c819486e75c14fd0615b
Zhanwei Wu <zhanweiw@qti.qualcomm.com>
2024-12-20 12:08:12 +0800

    Update code for support Linux & update sample code & docs

--- samples/python/real_esrgan_x4plus/real_esrgan_x4plus.py
+++ samples/python/real_esrgan_x4plus/real_esrgan_x4plus.py
@@ -116,7 +116,7 @@ def Release():
 
 Init()
 
-Inference(execution_ws + "\\input.png", execution_ws + "\\output.png")
+Inference(execution_ws + "\\input.jpg", execution_ws + "\\output.jpg")
 
 Release()
 
