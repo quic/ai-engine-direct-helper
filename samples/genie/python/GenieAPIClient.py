0964cd4d324d1c1f35e3358db5c54dec4400c092
quic-zhanweiw <quic_zhanweiw@quicinc.com>
2025-05-28 13:28:46 +0800

    Support download tokenizer automatically.

--- samples/genie/python/GenieAPIClient.py
+++ samples/genie/python/GenieAPIClient.py
@@ -18,6 +18,7 @@ PORT="8910"
 parser = argparse.ArgumentParser()
 parser.add_argument("--stream", action="store_true")
 parser.add_argument("--prompt", default="你好", type=str)
+parser.add_argument("--model", default="IBM-Granite-v3.1-8B", type=str)
 args = parser.parse_args()
 
 client = OpenAI(base_url="http://" + HOST + ":" + PORT + "/v1", api_key="123")
@@ -28,8 +29,7 @@ client = OpenAI(base_url="http://" + HOST + ":" + PORT + "/v1", api_key="123")
 messages = [{"role": "system", "content": "You are a math teacher who teaches algebra."}, {"role": "user", "content": args.prompt}]
 extra_body = {"size": 4096, "seed": 146, "temp": 1.5, "top_k": 13, "top_p": 0.6, "penalty_last_n": 64, "penalty_repeat": 1.3}
 
-model_name = "IBM-Granite-v3.1-8B"
-# model_name = "Phi-3.5-mini"
+model_name = args.model
 
 if args.stream:
     response = client.chat.completions.create(model=model_name, stream=True, messages=messages, extra_body=extra_body)
