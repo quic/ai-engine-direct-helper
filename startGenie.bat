45d44a67301cc33d0ec1fd158d8c2aeb9c5a6eb5
Slowisfast <zhuxiaodongzxd@gmail.com>
2025-06-02 14:49:26 +0800

    Add StartGenie.bat
    One click to launch Genie service and WebUI

---
+++ startGenie.bat
@@ -0,0 +1,4 @@
+D:
+cd \DiskD\GitHub\ai-engine-direct-helper\samples
+start /B GenieAPIService\GenieAPIService.exe -c "genie\python\models\IBM-Granite-v3.1-8B\config.json"  -l
+python webui\GenieWebUI.py
