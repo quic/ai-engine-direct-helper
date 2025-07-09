2f84c59f2f93abb8f6410ce3e2413e0fea345732
quic-zhanweiw <quic_zhanweiw@quicinc.com>
2025-07-09 23:37:44 +0800

    Update QAI_Launcher

--- tools/launcher/6.Start_LangFlow.bat
+++ tools/launcher/6.Start_LangFlow.bat
@@ -1,7 +1,7 @@
 @echo off
 set "currentDir=%CD%"
 
-set TOOL_PATH=%currentDir%\tools\pixi;%currentDir%\tools\aria2c;%currentDir%\tools\aria2c\aria2-1.36.0-win-64bit-build1;%currentDir%\tools\wget;%currentDir%\tools\Git\bin;
+set TOOL_PATH=%currentDir%\tools\pixi;%currentDir%\tools\aria2c;%currentDir%\tools\wget;%currentDir%\tools\Git\bin;
 set PATH=%TOOL_PATH%%PATH%
 
 echo Start langflow...
--- tools/launcher/utils/Install_Helper.py
+++ tools/launcher/utils/Install_Helper.py
@@ -85,9 +85,7 @@ def install_tools():
     wget_zip_path = tool_path + "\\wget.zip"
     wget_exe_path = wget_path + "\\wget.exe"
     if not os.path.exists(wget_exe_path):
-        fail = f"Failed to download tool from '{WGET_URL}'. Please download it manually and unzip it to '{tool_path}'. " + TEXT_RUN_SCRIPT_AGAIN
-        desc = f"Downloading '{WGET_URL}' to {wget_path}"
-        ret = download_url_pywget(WGET_URL, wget_zip_path, desc=desc, fail=fail)
+        ret = download_file_with_progress(WGET_URL, wget_zip_path)
         if not ret:
             exit()
         print(f"Install 'wget.exe' to {wget_exe_path}")
@@ -138,7 +136,7 @@ def ensure_windows_tools():
 
 def download_with_aria2c(url, dest_path, proxy=None):
     try:
-        cmd = ["aria2c", "-x", "16", "-s", "16", "-o", os.path.basename(dest_path), "-d", os.path.dirname(dest_path), url]
+        cmd = ["aria2c", "--console-log-level=error", "-x", "16", "-s", "16", "-o", os.path.basename(dest_path), "-d", os.path.dirname(dest_path), url]
         if proxy:
             cmd.extend(["--all-proxy", proxy])
         cmd.append("--continue=true")
