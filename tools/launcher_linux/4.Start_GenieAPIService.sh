79cd72bfec46cc4412ec8a849a363d027a504776
Ricky Li <wanjli@qti.qualcomm.com>
2026-01-08 14:28:40 +0800

    Update Linux sample doc and fix Linux python sample issue
    Signed-off-by: Ricky Li <wanjli@qti.qualcomm.com>

--- tools/launcher_linux/4.Start_GenieAPIService.sh
+++ tools/launcher_linux/4.Start_GenieAPIService.sh
@@ -5,7 +5,7 @@ scriptPath=$(pwd)
 # export LD_LIBRARY_PATH=$QNN_SDK_ROOT/lib/aarch64-oe-linux-gcc11.2
 # export ADSP_LIBRARY_PATH=$QNN_SDK_ROOT/lib/hexagon-v73/unsigned
 # 检查 GenieAPIService 是否运行
-genie_pid=$(pgrep -f "Genie")
+genie_pid=$(pgrep -f "GenieAPIService.py")
 if [ -n "$genie_pid" ]; then
     echo "GenieAPIService is ready now."
 else
