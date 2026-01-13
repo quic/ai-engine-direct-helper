48c43d450118e3bbb804a1b3cde6dd5b9e86c5f1
quic-zhanweiw <quic_zhanweiw@quicinc.com>
2026-01-13 17:49:56 +0800

    Fix sample image input data type issue.

--- samples/linux/python/convnext_base/convnext_base.py
+++ samples/linux/python/convnext_base/convnext_base.py
@@ -145,7 +145,7 @@ def Inference(input_image_path):
     PerfProfile.SetPerfProfileGlobal(PerfProfile.BURST)
 
     # Run the inference.
-    output_data = convnext_base.Inference([image])
+    output_data = convnext_base.Inference(image)
 
     # Reset the HTP.
     PerfProfile.RelPerfProfileGlobal()
