5ab26d810027212e3d872c01d06ee6db1081328f
Bob Zhou <bobozhou@qti.qualcomm.com>
2026-04-03 09:15:50 +0800

    Add SPDX license header to yolo26n_det python samples
    Signed-off-by: Bob Zhou <bobozhou@qti.qualcomm.com>

--- samples/linux/python/yolo26n_det/yolo26n_det.py
+++ samples/linux/python/yolo26n_det/yolo26n_det.py
@@ -1,5 +1,5 @@
 # ---------------------------------------------------------------------
-# Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+# Copyright (c) 2026 Qualcomm Innovation Center, Inc. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause
 # ---------------------------------------------------------------------
 
